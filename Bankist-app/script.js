a6054d5f7a3b03d603412704994f6629620f15f7
sp863 <sp863429@gmail.com>
2021-12-14 04:01:31 +0900

    bankist : balance

--- Bankist-app/script.js
+++ Bankist-app/script.js
@@ -79,6 +79,13 @@ const displayMovements = function (movements) {
 
 displayMovements(account1.movements);
 
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance} EUR`;
+};
+
+calcDisplayBalance(account1.movements);
+
 const createUserNames = function (accs) {
   accs.forEach(function (acc) {
     acc.username = acc.owner
@@ -89,7 +96,11 @@ const createUserNames = function (accs) {
   });
 };
 createUserNames(accounts);
-console.log(accounts);
+
+const calcPrintBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance} EUR`;
+};
 
 // console.log(containerMovements.innerHTML);
 /////////////////////////////////////////////////
