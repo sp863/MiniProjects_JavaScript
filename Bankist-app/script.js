dff113fa0d15d088eaa27b52bdb50e2021744d51
sp863 <sp863429@gmail.com>
2021-12-15 01:59:45 +0900

    bankist : close

--- Bankist-app/script.js
+++ Bankist-app/script.js
@@ -167,3 +167,22 @@ btnTransfer.addEventListener("click", function (e) {
     updateUI(currentAccount);
   }
 });
+
+btnClose.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (
+    currentAccount.username === inputCloseUsername.value &&
+    currentAccount.pin === Number(inputClosePin.value)
+  ) {
+    const index = accounts.findIndex(
+      (acc) => acc.username === currentAccount.username
+    );
+    console.log(index);
+    // Delete account
+    accounts.splice(index, 1);
+
+    // Hide UI
+    containerApp.style.opacity = 100;
+  }
+  inputCloseUsername.value = inputClosePin.value = "";
+});
