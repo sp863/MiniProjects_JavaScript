286d785008a582231fe4b23921a7f400dbc44c4a
sp863 <sp863429@gmail.com>
2021-12-17 14:28:32 +0900

    bankist web : scrolling2

--- Bankist-web/script.js
+++ Bankist-web/script.js
@@ -7,6 +7,8 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
 
 const openModal = function (e) {
   e.preventDefault();
@@ -29,22 +31,23 @@ document.addEventListener("keydown", function (e) {
   }
 });
 
-const btnScrollTo = document.querySelector(".btn--scroll-to");
-const section1 = document.querySelector("#section--1");
-
+//////////////////////////////////////////////
+// Button Scrolling
 btnScrollTo.addEventListener("click", function (e) {
-  const s1coords = section1.getBoundingClientRect();
-  console.log(s1coords);
+  // Works in modern browsers
+  section1.scrollIntoView({ behavior: "smooth" });
+  // const s1coords = section1.getBoundingClientRect();
+  // console.log(s1coords);
 
-  console.log(e.target.getBoundingClientRect()); // need these coordinates to tell js where to scroll to
+  // console.log(e.target.getBoundingClientRect()); // need these coordinates to tell js where to scroll to
 
-  console.log("Current scroll (X/Y)", window.pageXOffset, window.pageYOffset);
+  // console.log("Current scroll (X/Y)", window.pageXOffset, window.pageYOffset);
 
-  console.log(
-    "height/width viewport",
-    document.documentElement.clientHeight,
-    document.documentElement.clientWidth
-  );
+  // console.log(
+  //   "height/width viewport",
+  //   document.documentElement.clientHeight,
+  //   document.documentElement.clientWidth
+  // );
 
   // Scrolling
   // window.scrollTo(
@@ -57,7 +60,27 @@ btnScrollTo.addEventListener("click", function (e) {
   //   top: s1coords.top + window.pageYOffset,
   //   behavior: "smooth",
   // });
+});
 
-  // Works in modern browsers
-  section1.scrollIntoView({ behavior: "smooth" });
+//////////////////////////////////////////////
+// Page Navigation
+// document.querySelectorAll(".nav__link").forEach(function (el) {
+//   el.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute("href");
+//     // console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+//   });
+// });
+
+// using event delegation
+// 1. Add event listener to common parent element
+// 2. Determine what element originated the event
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+  // Matching strategy
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
 });
