987b14fe864f7af22520e46e9b1ebd816dca6bdf
sp863 <sp863429@gmail.com>
2021-12-17 19:00:55 +0900

    bankit web : sticky nav

--- Bankist-web/script.js
+++ Bankist-web/script.js
@@ -109,7 +109,6 @@ tabsContainer.addEventListener("click", function (e) {
 });
 
 // Menu fade animation
-
 const handleHover = function (e) {
   if (e.target.classList.contains("nav__link")) {
     const link = e.target;
@@ -127,3 +126,12 @@ nav.addEventListener("mouseover", handleHover.bind(0.5)); //mouseover bubbles, m
 
 //this value of '1' is passed
 nav.addEventListener("mouseout", handleHover.bind(1));
+
+// Sticky navigation
+const initialCoords = section1.getBoundingClientRect();
+console.log(initialCoords);
+window.addEventListener("scroll", function () {
+  console.log(window.scrollY);
+  if (window.scrollY > initialCoords.top) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+});
