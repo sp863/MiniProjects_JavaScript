e8c97961b062362050511468222efe9497d07751
sp863 <sp863429@gmail.com>
2021-12-19 05:58:52 +0900

    mapty : move to pop up

--- Mapty-app/script.js
+++ Mapty-app/script.js
@@ -5,6 +5,7 @@
 class Workout {
   date = new Date();
   id = (Date.now() + "").slice(-10);
+  clicks = 0;
   constructor(coords, distance, duration) {
     this.coords = coords; // [lat, lng]
     this.distance = distance; // in km
@@ -13,6 +14,10 @@ class Workout {
   _setDescription() {
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${months[this.date.getMonth()]} ${this.date.getDate()}`
+  }
+
+  click() {
+    this.clicks++;
   }
 }
 
@@ -60,12 +65,14 @@ const inputElevation = document.querySelector(".form__input--elevation");
 
 class App {
   #map;
+  #mapZoomLevel = 13;
   #mapEvent;
   #workouts = [];
   constructor() {
     this._getPosition();
     form.addEventListener("submit", this._newWorkout.bind(this));
     inputType.addEventListener("change", this._toggleElevationField);
+    containerWorkouts.addEventListener("click", this._moveToPopup.bind(this));
   }
   _getPosition() {
     if (navigator.geolocation) {
@@ -84,7 +91,7 @@ class App {
 
     const coords = [latitude, longitude];
 
-    this.#map = L.map("map").setView(coords, 13);
+    this.#map = L.map("map").setView(coords, this.#mapZoomLevel);
 
     L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
       attribution:
@@ -138,11 +145,10 @@ class App {
         // !Number.isFinite(cadence)
         !validInputs(distance, duration, cadence) ||
         !allPositive(distance, duration, cadence)
-      )
+      ) {
         return alert("Inputs have to be positive numbers!");
-
+      }
       workout = new Running([lat, lng], distance, duration, cadence);
-      this.#workouts.push(workout);
     }
     // if workout cycling, create cycling object
     if (type === "cycling") {
@@ -153,14 +159,15 @@ class App {
           duration,
           elevation || !allPositive(distance, duration)
         )
-      )
+      ) {
         return alert("Inputs have to be positive numbers!");
+      }
       workout = new Cycling([lat, lng], distance, duration, elevation);
     }
     // add new object to workout array
+    this.#workouts.push(workout);
     // render workout on map as marker
     this._renderWorkoutMarker(workout);
-
     // Render workout on list
     this._renderWorkout(workout);
     // Clear input fields
@@ -229,6 +236,27 @@ class App {
       </li>`;
     }
     form.insertAdjacentHTML("afterend", html);
+  }
+
+  _moveToPopup(e) {
+    const workoutEl = e.target.closest(".workout");
+    // console.log(workoutEl);
+
+    if (!workoutEl) return;
+
+    const workout = this.#workouts.find(
+      (work) => work.id === workoutEl.dataset.id
+    );
+    // console.log(workout);
+
+    this.#map.setView(workout.coords, this.#mapZoomLevel, {
+      animate: true,
+      pan: {
+        duration: 1,
+      },
+    });
+    //using the public interface
+    workout.click();
   }
 }
 
