ba0ce4024f9602d2d03f3a1a3b48487e3ed9bdea
sp863 <sp863429@gmail.com>
2021-12-19 06:23:51 +0900

    mapty : final

--- Mapty-app/script.js
+++ Mapty-app/script.js
@@ -69,7 +69,11 @@ class App {
   #mapEvent;
   #workouts = [];
   constructor() {
+    // Get user's position
     this._getPosition();
+    // Get data from local storage
+    this._getLocalStorage();
+    // Attach event handlers
     form.addEventListener("submit", this._newWorkout.bind(this));
     inputType.addEventListener("change", this._toggleElevationField);
     containerWorkouts.addEventListener("click", this._moveToPopup.bind(this));
@@ -100,6 +104,10 @@ class App {
 
     // Handling clicks on map
     this.#map.on("click", this._showForm.bind(this));
+
+    this.#workouts.forEach((work) => {
+      this._renderWorkoutMarker(work);
+    });
   }
   _showForm(mapE) {
     this.#mapEvent = mapE;
@@ -172,6 +180,8 @@ class App {
     this._renderWorkout(workout);
     // Clear input fields
     this._hideForm();
+    //Set local storage to all workouts
+    this._setLocalStorage();
   }
 
   _renderWorkoutMarker(workout) {
@@ -256,7 +266,25 @@ class App {
       },
     });
     //using the public interface
-    workout.click();
+    // workout.click(); ----> causes problems because objects coming from local storage will not inherit workout or running or cycling
+  }
+  _setLocalStorage() {
+    localStorage.setItem("workouts", JSON.stringify(this.#workouts));
+  }
+
+  _getLocalStorage() {
+    const data = JSON.parse(localStorage.getItem("workouts"));
+    // console.log(data);
+    if (!data) return;
+
+    this.#workouts = data;
+    this.#workouts.forEach((work) => {
+      this._renderWorkout(work);
+    });
+  }
+  reset() {
+    localStorage.removeItem("workouts");
+    location.reload();
   }
 }
 
