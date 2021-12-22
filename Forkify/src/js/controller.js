f44eefba3b3ab59e9b17dfeb9132a7083cda4cce
sp863 <sp863429@gmail.com>
2021-12-22 22:20:15 +0900

    forkify : search results

--- Forkify/src/js/controller.js
+++ Forkify/src/js/controller.js
@@ -1,16 +1,16 @@
 import * as model from "./model.js";
 import recipeView from "./views/recipeView.js";
+import searchView from "./views/searchView.js";
+import resultsView from "./views/resultsView.js";
 
 // import icons from '../img/icons.svg'; // Parcel 1
 
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 
-const recipeContainer = document.querySelector(".recipe");
-
-// https://forkify-api.herokuapp.com/v2
-
-///////////////////////////////////////
+if (module.hot) {
+  module.hot.accept();
+}
 
 const controlRecipes = async function () {
   try {
@@ -28,9 +28,24 @@ const controlRecipes = async function () {
     recipeView.renderError();
   }
 };
-controlRecipes();
+
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();
+    // 1) Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+    // 2) Load search results
+    await model.loadSearchResults(query);
+    // 3) Render results
+    resultsView.render(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 };
-init(); //xx
+init();
